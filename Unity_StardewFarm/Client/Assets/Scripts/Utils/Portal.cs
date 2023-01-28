c1827559326be4cdec59bb0b468c4da075fdc4fa
haegaea <raeweric@gmail.com>
2023-01-29 00:39:52 +0900

    변경사항 hold on 버그 수정 및 포탈 이름 변경

--- Unity_StardewFarm/Client/Assets/Scripts/Utils/Portal.cs
+++ Unity_StardewFarm/Client/Assets/Scripts/Utils/Portal.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Potal : MonoBehaviour
+public class Portal : MonoBehaviour
 {
     public enum NextPositionType
     {
@@ -14,22 +14,27 @@ public class Potal : MonoBehaviour
 
     public Transform DestinationPoint;
 
+    public bool Trans = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
-            if(nextPositionType == NextPositionType.InitPosition)
+            Trans = true;
+            if(nextPositionType == NextPositionType.InitPosition && Trans == true)
             {
                 collision.transform.position = Vector3.zero;
             }
             else if (nextPositionType == NextPositionType.SomePosition)
             {
                 collision.transform.position = DestinationPoint.position;
+                
             }
             else
             {
 
             }
+            Trans = false;
         }
     }
 }
